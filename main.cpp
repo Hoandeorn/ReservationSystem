0ab4d76f035db04759a23f22f8ff8bd9b660db16
SappleHalf <nesofhzhhj@icloud.com>
2022-03-25 18:51:14 +0800

    Revert "test2"
    This reverts commit d4f5561db079c7c010cecc9ca72359c1f249da5d.

--- main.cpp
+++ main.cpp
@@ -2,8 +2,6 @@
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
-    std::cout << "Add in master" << std::endl;
     std::cout << "add" << std::endl;
-    std::cout << "Cherry" << std::endl;
     return 0;
 }

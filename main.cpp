d4f5561db079c7c010cecc9ca72359c1f249da5d
SappleHalf <nesofhzhhj@icloud.com>
2022-03-25 18:47:21 +0800

    test2

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    std::cout << "Add in master" << std::endl;
     std::cout << "add" << std::endl;
+    std::cout << "Cherry" << std::endl;
     return 0;
 }
